892fdf997fe92512ba2820320516c89b8069fc03
warshophysical <73259613+warshophysical@users.noreply.github.com>
2021-04-07 13:11:58 -0500

    init() function

--- plots.js
+++ plots.js
@@ -1,4 +1,26 @@
-var selector = d3.select("#selDataset");
+
+// The function which is the function that writes Demographic Info Panel
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    // `#sample-metadata` selected
+    var PANEL = d3.select("#sample-metadata");
+
+    // cleared existing data
+    PANEL.html("");
+
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+
+  });
+}
+// Init function which is used to create initial page once index.html run
+// Init funtion call buildMeta function to create Demographic Info panel
+function init() {
+  var selector = d3.select("#selDataset");
 
   d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
@@ -9,5 +31,12 @@ var selector = d3.select("#selDataset");
         .text(sampleName)
         .property("value", sampleName);
     });
+
+    var firstSample = sampleNames[0];
+    buildMetadata(firstSample);
   });
+}
+
+init();
+
 
