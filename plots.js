643429c38c74887a38cd620234452e9e08d13be0
warshophysical <73259613+warshophysical@users.noreply.github.com>
2021-04-07 12:42:41 -0500

    loading data to selector as reference

--- plots.js
+++ plots.js
@@ -1,3 +1,13 @@
-d3.json("samples.json").then((data) => {
-  var sampleNames = data.names;
-});
+var selector = d3.select("#selDataset");
+
+  d3.json("samples.json").then((data) => {
+    var sampleNames = data.names;
+
+    sampleNames.forEach((sampleName) => {
+      selector
+        .append("option")
+        .text(sampleName)
+        .property("value", sampleName);
+    });
+  });
+
